7ba5d05085cb89a43ecba4ba9fb22902aac92588
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-17 14:58:34 -0300

    Editing Lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,31 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to
+  end
+
+
 end
