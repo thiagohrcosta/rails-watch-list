a1609008bb30a6bbba7d226376292594940c22ab
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-17 14:45:21 -0300

    Using resources to crate routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,4 +6,11 @@ class BookmarksController < ApplicationController
   def show
     @bookmark = Bookmark.find(params[:id])
   end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+  end
 end
