8727a87da765ad79b0615ad2875b23ec67612ec7
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-17 15:13:27 -0300

    Fix some typo error on route

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,29 +3,4 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
-
-  def new
-    @list = List.new
-  end
-
-  def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to
-    else
-      render :new
-    end
-  end
-
-  def delete
-    @list = List.find(params[:id])
-    @list.destroy
-
-    redirect_to
-  end
-
-
 end
