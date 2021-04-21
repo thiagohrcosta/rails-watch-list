3983f35835fc241a419f731da9ea6a33e4140a08
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-20 22:29:49 -0300

    Adding CSS to show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :lists do
     resources :bookmarks
   end
+
+  resources :movies
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,23 @@
 
 Movie.create!(
   title: "Wonder Woman 1984",
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
   rating: 6.9
+  )
+
+Movie.create!(
+  title: "Justice League",
+  overview: "Fuelled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne and Diana Prince assemble a team of metahumans consisting of Barry Allen, Arthur Curry and Victor Stone to face the catastrophic threat of Steppenwolf and the Parademons who are on the hunt for three Mother Boxes on Earth.",
+  poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/eifGNCSDuxJeS1loAXil5bIGgvC.jpg",
+  rating: 6.2
+  )
+
+Movie.create!(
+  title: "Batman V Superman: Dawn of Justice",
+  overview: "Fearing the actions of a god-like Super Hero left unchecked, Gotham City’s own formidable, forceful vigilante takes on Metropolis’s most revered, modern-day savior, while the world wrestles with what sort of hero it really needs. And with Batman and Superman at war with one another, a new threat quickly arises, putting mankind in greater danger than it’s ever known before.",
+  poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5UsK3grJvtQrtzEgqNlDljJW96w.jpg",
+  rating: 5.9
   )
 
 Movie.create!(
