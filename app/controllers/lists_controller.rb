5e7c4f58defe2e704a068f0be0e1e17065a26d35
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-21 21:10:41 -0300

    Adding some CSS

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -37,6 +37,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name, :cover, :photo)
+    params.require(:list).permit(:name, :photo)
   end
 end
